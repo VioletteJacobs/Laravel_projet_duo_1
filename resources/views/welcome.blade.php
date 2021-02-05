53eea649432c9dbdadb2181d8ca31872aa2e6f6a
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-02-05 09:10:02 +0100

    controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img">
-          <img src="img/hero-img.png'" class="img-fluid" alt="">
+          <img src="{{asset('img/hero-img.png')}}'" class="img-fluid" alt="">
         </div>
       </div>
     </div>
@@ -36,7 +36,7 @@
             <h2>Eum ipsam laborum deleniti velitena</h2>
             <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
             {{-- rajouter une image --}}
-            <img class="img-fluid w-25" src="../img/mer.jpeg" alt="">
+            <img class="img-fluid w-25" src="../img/mer.jpeg" alt="photo de bateau sur la mer">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
             <p>
